915dbc7469a502de8b27c91f82aff807157bf961
Miner.Alex <qingqianshui0@gmail.com>
2025-09-27 16:05:35 +0000

    consistenthash

---
+++ src/consistenthash/consistenthash_test.go
@@ -0,0 +1,41 @@
+package consistenthash
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestHashing(t *testing.T) {
+	// new a Map instance
+	hash := New(3, func(key []byte) uint32 {
+		i, _ := strconv.Atoi(string(key))
+		return uint32(i)
+	})
+
+	hash.Add("6", "4", "2")
+
+	// key, and node name expected to return
+	testCases := map[string]string{
+		"2":"2",
+		"11":"2",
+		"23":"4",
+		"27":"2",
+	}
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+		t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+	// change nodes
+	hash.Add("8")
+
+	testCases["27"] = "8"
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+		t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+}
